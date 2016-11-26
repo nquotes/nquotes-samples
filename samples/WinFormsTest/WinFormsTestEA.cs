a0acdec300948acacfd9f5f5e75045f82448ac5b
battlmonstr <battlmonstr@localhost>
2016-11-26 17:17:51 +0100

    0000493: update WinFormsTest sample to have an event loop
    The UI button should not run API calls directly.
    Instead it posts a task to the EA thread.
    When the task finishes, it returns to the UI thread via Invoke and updates the UI.

--- samples/WinFormsTest/WinFormsTestEA.cs
+++ samples/WinFormsTest/WinFormsTestEA.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Threading;
 using NQuotes;
 
 namespace TestExperts.WinFormsTest
 {
-    public class WinFormsTestEA : MqlApi
+    public class WinFormsTestEA : MqlApi, IMqlApiTaskRunner
     {
         private Thread _uiThread;
         private TestForm _form;
         private bool _isFormClosed;
+        private BlockingCollection<Action<IMqlApi>> _taskRunnerQueue;
 
         void StartUI()
         {
@@ -20,6 +22,8 @@ namespace TestExperts.WinFormsTest
 
         public override int init()
         {
+            _taskRunnerQueue = new BlockingCollection<Action<IMqlApi>>();
+
             // create and start a thread for UI 
             // this is needed to avoid blocking the terminal interaction with the MQL API
             _uiThread = new Thread(this.StartUI);
@@ -27,39 +31,29 @@ namespace TestExperts.WinFormsTest
             return 0;
         }
 
-        // A helper function to do UI updates.
-        // It's needed, because all UI calls must happen on the UI thread.
-        // Invoke() runs the action on that thread.
-        void InvokeUI(Action action)
+        void IMqlApiTaskRunner.Post(Action<IMqlApi> action)
         {
-            if ((_form != null) && !_isFormClosed)
-                _form.Invoke(action);
+            // add the task to the queue for later execution from the EA thread in start()
+            _taskRunnerQueue.Add(action);
         }
 
         public override int start()
         {
-            int elapsedSeconds = 0;
-
             // waiting loop until the form is closed
             // the MQL API in the UI thread will work while we are waiting here
             // (without this loop the calls happening between ticks would be blocked waiting for the next tick)
             while (!IsStopped() && !_isFormClosed)
             {
-                Sleep(1000);
-                elapsedSeconds++;
-
-                // call MQL API "TimeLocal" function
-                DateTime localTime = TimeLocal();
-                string status = String.Format("Waiting for {0} sec", elapsedSeconds);
-
-                // perform a UI update
-                // use Invoke(), because all UI calls must happen on the UI thread
-                InvokeUI(() =>
-                {
-                    _form.SetLocalTime(localTime);
-                    _form.SetStatus(status);
-                });
+                // execute a task posted from the UI
+                Action<IMqlApi> action;
+                if (_taskRunnerQueue.TryTake(out action, TimeSpan.FromSeconds(1)))
+                    action(this);
             }
+
+            // automatically remove EA from the chart when the form is closed
+            if (!IsStopped())
+                ExpertRemove();
+
             return 0;
         }
 
@@ -68,7 +62,8 @@ namespace TestExperts.WinFormsTest
             // make sure that the form is closed
             // use Invoke(), because all UI calls must happen on the UI thread
             // after the form is closed the UI thread finishes
-            InvokeUI(() => _form.Close());
+            if ((_form != null) && !_isFormClosed)
+                _form.Invoke(new Action(_form.Close));
             return 0;
         }
     }
