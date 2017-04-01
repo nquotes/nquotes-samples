d6402af3b9ee42b1d319cda64b03c2e7f9e43f77
battlmonstr <battlmonstr@users.noreply.github.com>
2017-04-01 22:04:38 +0200

    WPF UI sample

---
+++ samples/WPFFormTest/TestForm.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Timers;
+using System.Windows;
+using NQuotes;
+
+namespace WPFFormTest
+{
+    public interface IMqlApiTaskRunner
+    {
+        void Post(Action<IMqlApi> action);
+    }
+
+    public partial class TestForm : Window
+    {
+        private readonly IMqlApiTaskRunner _apiRunner;
+        private readonly Stopwatch _stopwatch;
+        private readonly Timer _statusTimer;
+
+        public TestForm(IMqlApiTaskRunner apiRunner)
+        {
+            _apiRunner = apiRunner;
+            _stopwatch = Stopwatch.StartNew();
+            InitializeComponent();
+
+            _statusTimer = new Timer(1000);
+            _statusTimer.Elapsed += statusTimer_Tick;
+            _statusTimer.Start();
+        }
+
+        private void getInfoButton_Click(object sender, EventArgs e)
+        {
+            _apiRunner.Post((IMqlApi api) =>
+                {
+                    // call MQL API functions
+                    string symbol = api.Symbol();
+                    string terminalCompany = api.TerminalCompany();
+
+                    // perform a UI update
+                    // use InvokeAsync(), because all UI calls must happen on the UI thread
+                    Dispatcher.InvokeAsync(new Action(() =>
+                        {
+                            symbolLabel.Content = symbol;
+                            brokerCompanyLabel.Content = terminalCompany;
+                        }));
+                });
+        }
+
+        private void statusTimer_Tick(object sender, EventArgs e)
+        {
+            _apiRunner.Post((IMqlApi api) =>
+                {
+                    // call MQL API "TimeLocal" function
+                    DateTime localTime = api.TimeLocal();
+
+                    // perform a UI update
+                    // use InvokeAsync(), because all UI calls must happen on the UI thread
+                    Dispatcher.InvokeAsync(new Action(() =>
+                        {
+                            localTimeLabel.Content = localTime.ToLongTimeString();
+                            statusLabel.Content = String.Format("Running for {0:mm\\:ss} sec", _stopwatch.Elapsed);
+                        }));
+                });
+        }
+
+        private void window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _statusTimer.Stop();
+            _stopwatch.Stop();
+        }
+    }
+}
---
+++ samples/WPFFormTest/WPFFormTestEA.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Windows;
+using NQuotes;
+
+namespace WPFFormTest
+{
+    public class WPFFormTestEA : MqlApi, IMqlApiTaskRunner
+    {
+        private Thread _uiThread;
+        private TestForm _form;
+        private bool _isFormClosed;
+        private BlockingCollection<Action<IMqlApi>> _taskRunnerQueue;
+
+        void StartUI()
+        {
+            // show a form and wait until it's closed
+            _form = new TestForm(this);
+            Application app = new Application();
+            app.Run(_form);
+            _isFormClosed = true;
+        }
+
+        public override int init()
+        {
+            _taskRunnerQueue = new BlockingCollection<Action<IMqlApi>>();
+
+            // create and start a thread for UI 
+            // this is needed to avoid blocking the terminal interaction with the MQL API
+            _uiThread = new Thread(this.StartUI);
+            _uiThread.SetApartmentState(ApartmentState.STA);
+            _uiThread.Start();
+            return 0;
+        }
+
+        void IMqlApiTaskRunner.Post(Action<IMqlApi> action)
+        {
+            // add the task to the queue for later execution from the EA thread in start()
+            _taskRunnerQueue.Add(action);
+        }
+
+        public override int start()
+        {
+            // waiting loop until the form is closed
+            // the MQL API in the UI thread will work while we are waiting here
+            // (without this loop the calls happening between ticks would be blocked waiting for the next tick)
+            while (!IsStopped() && !_isFormClosed)
+            {
+                // execute a task posted from the UI
+                Action<IMqlApi> action;
+                if (_taskRunnerQueue.TryTake(out action, TimeSpan.FromSeconds(1)))
+                    action(this);
+            }
+
+            // automatically remove EA from the chart when the form is closed
+            if (!IsStopped())
+                ExpertRemove();
+
+            return 0;
+        }
+
+        public override int deinit()
+        {
+            // make sure that the form is closed
+            // use Invoke(), because all UI calls must happen on the UI thread
+            // after the form is closed the UI thread finishes
+            if ((_form != null) && !_isFormClosed)
+                _form.Dispatcher.Invoke(new Action(_form.Close));
+            return 0;
+        }
+    }
+}
