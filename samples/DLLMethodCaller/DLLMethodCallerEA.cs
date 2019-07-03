bd88ed191b409d8eec18b583bf51ff9681af2e97
battlmonstr <battlmonstr@users.noreply.github.com>
2019-07-03 19:55:54 +0200

    FortuneTeller stub

--- samples/DLLMethodCaller/DLLMethodCallerEA.cs
+++ samples/DLLMethodCaller/DLLMethodCallerEA.cs
@@ -7,15 +7,35 @@ namespace DLLMethodCaller
 {
     public class DLLMethodCallerEA : MqlApi
     {
+        public string ActionName { get; set; }
+
+        // GiveMoney action
+        public int MoneyAmount { get; set; }
+
+        // PredictFuture action
+        public DateTime MyBirthday { get; set; }
+        public bool AmMarried { get; set; }
+        public string FuturePrediction { get; set; }
+
+        private FortuneTeller fortuneTeller;
+
         public override int init()
         {
-            // TODO: implement initialization ( https://docs.mql4.com/runtime/event_fire#init )
+            fortuneTeller = new FortuneTeller();
             return 0;
         }
 
         public override int start()
         {
-            // TODO: implement OnTick logic ( https://docs.mql4.com/runtime/event_fire#newtick )
+            switch (ActionName)
+            {
+                case "GiveMoney":
+                    fortuneTeller.AcceptMoney(MoneyAmount);
+                    break;
+                case "PredictFuture":
+                    FuturePrediction = fortuneTeller.PredictFuture(MyBirthday, AmMarried);
+                    break;
+            }
             return 0;
         }
 
---
+++ samples/DLLMethodCaller/FortuneTeller.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DLLMethodCaller
+{
+    class FortuneTeller
+    {
+        public void AcceptMoney(int amount)
+        {
+            // TODO
+        }
+
+        public string PredictFuture(DateTime birthday, bool isMarried)
+        {
+            // TODO
+            return "";
+        }
+    }
+}
