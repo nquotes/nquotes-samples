9b284dec08e87e9750540c3407b8c2008958cc1e
battlmonstr <battlmonstr@users.noreply.github.com>
2019-07-03 19:28:03 +0200

    new empty sample project

---
+++ samples/DLLMethodCaller/DLLMethodCallerEA.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NQuotes;
+
+namespace DLLMethodCaller
+{
+    public class DLLMethodCallerEA : MqlApi
+    {
+        public override int init()
+        {
+            // TODO: implement initialization ( https://docs.mql4.com/runtime/event_fire#init )
+            return 0;
+        }
+
+        public override int start()
+        {
+            // TODO: implement OnTick logic ( https://docs.mql4.com/runtime/event_fire#newtick )
+            return 0;
+        }
+
+    }
+}
