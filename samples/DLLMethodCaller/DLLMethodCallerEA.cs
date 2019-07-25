6c7253655c4a820e7df5600172ce17de868d524b
ashmetry <ash@ametry.com>
2019-07-25 18:40:00 -0500

    Added FortuneTeller

---
+++ samples/DLLMethodCaller/DLLMethodCallerEA.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NQuotes;
+
+namespace DLLMethodCaller
+{
+    public class DLLMethodCallerEA : MqlApi
+    {
+        [ExternVariable]
+        public string ActionName { get; set; }
+
+        // GiveMoney action
+        [ExternVariable]
+        public int MoneyAmount { get; set; }
+        // PredictFuture action
+        [ExternVariable]
+        public DateTime MyBirthday { get; set; }
+        [ExternVariable]
+        public bool AmMarried { get; set; }
+        //[ExternVariable]
+        public string FuturePrediction { get; set; }
+
+        private FortuneTeller fortuneTeller;
+        
+        public override int init()
+        {
+            fortuneTeller = new FortuneTeller();
+            FuturePrediction = "Unknown";
+            return 0;
+        }
+
+        public override int start()
+        {
+            switch (ActionName)
+            {
+                case "GiveMoney":
+                    fortuneTeller.AcceptMoney(MoneyAmount);
+                    break;
+                case "PredictFuture":
+                    FuturePrediction = fortuneTeller.PredictFuture(MyBirthday, AmMarried);
+                    break;
+            }
+            return 0;
+        }
+
+    }
+}
---
+++ samples/DLLMethodCaller/FortuneTeller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DLLMethodCaller
+{
+    class FortuneTeller
+    {
+        private int total;
+        public void AcceptMoney(int amount)
+        {
+            total += amount;
+        }
+
+        public string PredictFuture(DateTime birthday, bool isMarried)
+        {
+            if (isMarried)
+                return "You're broke!";
+
+            Random random = new Random(int.Parse(birthday.ToString("yyyyMMdd")));
+            lock (random) // synchronize
+            {
+                return $"You're rich, You're worth {random.Next(0, total)} today!";
+            }
+            return "";
+        }
+    }
+}
