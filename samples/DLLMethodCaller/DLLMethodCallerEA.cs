249456aeff042512314f05c0060c2b67b7d87573
battlmonstr <battlmonstr@users.noreply.github.com>
2019-07-29 18:42:06 +0200

    DLL method call sample (#9)

---
+++ samples/DLLMethodCaller/DLLMethodCallerEA.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NQuotes;
+
+namespace DLLMethodCaller
+{
+    public class DLLMethodCallerEA : MqlApi
+    {
+        public string ActionName { get; set; }
+
+        // GiveMoney action parameters
+        public int MoneyAmount { get; set; }
+
+        // PredictFuture action parameters
+        public DateTime MyBirthday { get; set; }
+        public bool AmMarried { get; set; }
+        public string FuturePrediction { get; set; }
+
+        private FortuneTeller fortuneTeller;
+        
+        public override int init()
+        {
+            fortuneTeller = new FortuneTeller();
+            FuturePrediction = "Unknown";
+            return 0;
+        }
+
+        public override int start()
+        {
+            switch (ActionName)
+            {
+                case "GiveMoney":
+                    fortuneTeller.AcceptMoney(MoneyAmount);
+                    break;
+                case "PredictFuture":
+                    FuturePrediction = fortuneTeller.PredictFuture(MyBirthday, AmMarried);
+                    break;
+            }
+            return 0;
+        }
+    }
+}
---
+++ samples/DLLMethodCaller/FortuneTeller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DLLMethodCaller
+{
+    class FortuneTeller
+    {
+        private int total;
+        
+        public void AcceptMoney(int amount)
+        {
+            total += amount;
+        }
+
+        public string PredictFuture(DateTime birthday, bool isMarried)
+        {
+            if (total <= 0)
+                return "Pay me some money first.";
+            total = 0;
+
+            if (isMarried)
+                return "You're broke!"; 
+
+            var random = new Random(birthday.Year + birthday.Month + birthday.Day);
+            bool willBeRich = (random.Next() % 2) == 0;
+
+            if (willBeRich)
+                return "You're going to be rich! Well done!";
+            else
+                return "You're not going to be rich, but also not broke. That's a good thing.";
+        }
+    }
+}
